65d27f406ce2cc78a8daa417aac5e2846427440a
Astro <astro@spaceboyz.net>
2010-12-15 22:06:50 +0100

    Synth: make generate stateful for readability

--- src-hs/Synth.hs
+++ src-hs/Synth.hs
@@ -6,6 +6,7 @@ import Data.Word (Word16)
 import Data.Array
 import Data.Bits
 import Data.Word
+import Control.Monad.State.Lazy
 
 
 data ChannelState = Release | Attack | Hold
@@ -91,73 +92,72 @@ play s = do synth <- readIORef s
 mix :: Synthesizer -> (Synthesizer, Double, Double)
 mix synth = let channels = synthChannels synth
                 (channels', l, r) = foldl (\(cs, l, r) (idx, channel) ->
-                                           let (channel', l', r') = generate channel
+                                           let ((l', r'), channel') = runState generate channel
                                            in ((idx, channel') : cs, l + l', r + r')
                                          ) ([], 0, 0) $ assocs channels
                 channels'' = array (bounds channels) channels'
             in (synth { synthChannels = channels'' }, l, r)
 
-generate :: Channel -> (Channel, Double, Double)
-generate = osc . adsr
-    where adsr :: Channel -> Channel
-          adsr c = case chState c of
+generate :: State Channel (Double, Double)
+generate = adsr >> osc
+    where adsr :: State Channel ()
+          adsr = do c <- get
+                    case chState c of
                      Release ->
-                         c { chLevel = chLevel c * chRelease c }
+                         put $ c { chLevel = chLevel c * chRelease c }
                      Attack ->
                          let level = chLevel c + chAttack c
                          in if level > 1
-                            then c { chLevel = 1,
-                                     chState = Hold }
-                            else c { chLevel = level }
+                            then put $ c { chLevel = 1,
+                                           chState = Hold }
+                            else put $ c { chLevel = level }
                      Hold ->
-                         c { chLevel = chSustain c + (chLevel c - chSustain c) * chDecay c }
-          osc :: Channel -> (Channel, Double, Double)
-          osc c = let c' = c { chPhase = chPhase c + chSpeed c }
-                      c'' = case chWave c' of 
-                              Noise ->
-                                  c'
-                              _ ->
-                                  c' { chPhase = chPhase c' -
-                                                 fromIntegral (truncate $ chPhase c') }
-                      (amp, c''') = case chWave c'' of
-                                      Pulse ->
-                                          (if chPhase c'' < chPulseWidth c''
-                                           then -1
-                                           else 1, 
-                                           c'')
-                                      Triangle ->
-                                          (if chPhase c'' < chPulseWidth c''
-                                           then (2 / chPulseWidth c'') * chPhase c'' - 1
-                                           else (-2 / (1 - chPulseWidth c'')) * 
-                                                    (chPhase c'' - chPulseWidth c'') + 1,
-                                           c'')
-                                      Sine ->
-                                          (sin $ chPhase c'' * 2 * pi,
-                                           c'')
-                                      Noise ->
-                                          let r :: Word32 -> Word32 -> Double -> (Word32, Word32, Double)
-                                              r s b phase
-                                                  | phase > 0.1 =
-                                                      let phase' = phase - 0.1
-                                                          b' = ((s `shiftR` 22) `xor` (s `shiftR` 17)) .&. 1
-                                                          s' = ((s `shiftL` 1) .&. 0x7fffff) + b'
-                                                      in r s' b' phase'
-                                                  | otherwise = 
-                                                      (s, b, phase)
-                                              (s, b, phase) = r (chShiftReg c'') 0 $ chPhase c''
-                                          in ((fromIntegral $
-                                               ((s .&. 0x400000) `shiftR` 11) .|.
-                                               ((s .&. 0x100000) `shiftR` 10) .|.
-					       ((s .&. 0x010000) `shiftR` 7) .|.
-					       ((s .&. 0x002000) `shiftR` 5) .|.
-					       ((s .&. 0x000800) `shiftR` 4) .|.
-					       ((s .&. 0x000080) `shiftR` 1) .|.
-					       ((s .&. 0x000010) `shiftL` 1) .|.
-					       ((s .&. 0x000004) `shiftL` 2)) *
-                                              (1.0 / (fromIntegral (1 `shiftL` 12 :: Word32))) - 0.5,
-                                              c'' { chPhase = phase,
-                                                    chShiftReg = s })
-
-                      amp' = amp * chVolume c''' * chLevel c'''
-                  in (c''', amp' * chPanLeft c''', amp' * chPanRight c''')
-                     
+                         put $ c { chLevel = chSustain c + (chLevel c - chSustain c) * chDecay c }
+          osc :: State Channel (Double, Double)
+          osc = do c <- get
+                   put $ c { chPhase = chPhase c + chSpeed c }
+                   
+                   c <- get
+                   when (chWave c /= Noise) $
+                        put $ c { chPhase = chPhase c -
+                                            fromIntegral (truncate $ chPhase c) }
+                   
+                   c <- get
+                   amp <- case chWave c of
+                            Pulse ->
+                                return $ if chPhase c < chPulseWidth c
+                                         then -1
+                                         else 1
+                            Triangle ->
+                                return $ if chPhase c < chPulseWidth c
+                                         then (2 / chPulseWidth c) * chPhase c - 1
+                                         else -2 / (1 - chPulseWidth c) * 
+                                              (chPhase c - chPulseWidth c) + 1
+                            Sine ->
+                                return $ sin $ chPhase c * 2 * pi
+                            Noise ->
+                                do let r :: Word32 -> Word32 -> Double -> (Word32, Word32, Double)
+                                       r s b phase
+                                           | phase > 0.1 =
+                                               let phase' = phase - 0.1
+                                                   b' = ((s `shiftR` 22) `xor` (s `shiftR` 17)) .&. 1
+                                                   s' = ((s `shiftL` 1) .&. 0x7fffff) + b'
+                                               in r s' b' phase'
+                                           | otherwise = 
+                                               (s, b, phase)
+                                       (s, b, phase) = r (chShiftReg c) 0 $ chPhase c
+                                   put $ c { chPhase = phase,
+                                             chShiftReg = s }
+                                   return $ (fromIntegral $
+                                             ((s .&. 0x400000) `shiftR` 11) .|.
+                                             ((s .&. 0x100000) `shiftR` 10) .|.
+                                             ((s .&. 0x010000) `shiftR` 7) .|.
+                                             ((s .&. 0x002000) `shiftR` 5) .|.
+                                             ((s .&. 0x000800) `shiftR` 4) .|.
+                                             ((s .&. 0x000080) `shiftR` 1) .|.
+                                             ((s .&. 0x000010) `shiftL` 1) .|.
+                                             ((s .&. 0x000004) `shiftL` 2)) *
+                                              (1.0 / (fromIntegral (1 `shiftL` 12 :: Word32))) - 0.5
+                   c <- get
+                   let amp' = amp * chVolume c * chLevel c
+                   return (amp' * chPanLeft c, amp' * chPanRight c)
